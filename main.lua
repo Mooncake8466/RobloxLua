4d627e5aac28888bfd0cd10c87a8b88b950bf117
Mooncake8466 <85029703+Mooncake8466@users.noreply.github.com>
2024-05-08 21:55:10 +0800

    Update and rename Backroom.lua to main.lua

---
+++ main.lua
@@ -0,0 +1,9 @@
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):Destroy()
+
+getgenv().AuthKey = "HUGE_Ifl36f3FXyTB"
+getgenv().LoadSettings = {
+    Example_Setting = Example_Value
+}
+loadstring(game:HttpGet("https://HugeGames.io/ps99"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mooncake8466/RobloxLua/main/backroom.lua"))()
