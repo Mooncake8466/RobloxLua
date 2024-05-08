29e07e1c007a92060f0b06d2b6eedbfaf7b09a68
Mooncake8466 <85029703+Mooncake8466@users.noreply.github.com>
2024-05-08 20:32:32 +0800

    Update Backroom.lua

--- Backroom.lua
+++ Backroom.lua
@@ -1,5 +1,11 @@
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):Destroy()
 
+getgenv().AuthKey = "HUGE_Ifl36f3FXyTB"
+getgenv().LoadSettings = {
+    Example_Setting = Example_Value
+}
+loadstring(game:HttpGet("https://HugeGames.io/ps99"))()
+
 --[[
  .____                  ________ ___.    _____                           __                
  |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
